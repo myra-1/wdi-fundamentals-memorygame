6cad1bdee2b57e641b82c3590c04b1e4a79c5b68
Myra Kornides <myra@Myras-MBP.lan1>
2020-02-16 22:27:02 -0500

    finished creating game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,22 +25,33 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again");
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage)
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i =0; i < cards.length; i++){
+	let cardElement = document.createElement('img');
+	cardElement.setAttribute('src', "images/back.png");
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click',flipCard);
+	document.getElementById('game-board').appendChild(cardElement);}
+};
+createBoard();
+
+
